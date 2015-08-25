6021244d427166ab0ccd5372f125b1b46d9cecbf
dis-zephyro <zephyro@yandex.ru>
2015-08-25 12:34:02 +0300

    v1

--- includes/footer.php
+++ includes/footer.php
@@ -42,6 +42,8 @@
     </div>
 </footer>
 
+<div class="hidden"></div>
+
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
